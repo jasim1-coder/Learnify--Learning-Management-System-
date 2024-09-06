1b3b25600e1db2ede0a52ab46fd0482595079f8d
jasim <tjasim786@gmail.com>
2024-09-06 09:44:29 +0530

    final backend

--- api/views.py
+++ api/views.py
@@ -1,5 +1,8 @@
-import random
 from django.shortcuts import render
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.conf import settings
+
 from api import serializer as api_serializer
 from userauths.models import User, Profile
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -7,6 +10,8 @@ from rest_framework import generics, status
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
+import random
+
 # Create your views here.
 
 class MyTokenObtainPairView(TokenObtainPairView):
@@ -40,6 +45,25 @@ class PasswordResetEmailVerifyAPIView(generics.RetrieveAPIView):
             user.otp = generate_random_otp()
             user.save()
             link = f"http://localhost:5173/create-new-password/?otp{user.otp}&uuidb64={uuidb64}&=refresh_token{refresh_token}"
+           
+            context = {
+                "link": link,
+                "username": user.username
+            }
+
+            subject = "Password Reset Email"
+            text_body = render_to_string("email/password_reset.txt", context)
+            html_body = render_to_string("email/password_reset.html", context)
+
+            msg = EmailMultiAlternatives(
+                subject=subject,
+                from_email=settings.FROM_EMAIL,
+                to=[user.email],
+                body=text_body
+            )
+            msg.attach_alternative(html_body, "text/html")
+            msg.send()
+
             print("link ======", link)  
 
         return user
--- backend/settings.py
+++ backend/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+from environs import Env
+env = Env()
+env.read_env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -51,7 +54,9 @@ INSTALLED_APPS = [
     #third party apps
 	'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
-    'corsheaders'
+    'corsheaders',
+    'anymail',
+    'drf_yasg',
 
 ]
 
@@ -141,6 +146,18 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = 'userauths.User'
+
+MAILGUN_SECRET_KEY = env("MAILGUN_SECRET_KEY")
+MAILERSEND_API_TOKEN = env("MAILERSEND_API_TOKEN")
+MAILGUN_SENDER_DOMAIN = env("MAILGUN_SENDER_DOMAIN")
+
+ANYMAIL = {
+    "MAILGUN_API_KEY": env("MAILGUN_API_KEY"),
+    "MAILGUN_SENDER_DOMAIN": env("MAILGUN_SENDER_DOMAIN"),
+}
+
+FROM_EMAIL = env("FROM_EMAIL")
+EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
